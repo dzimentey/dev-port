b38789c176b372cf0f6ab83873b80402db9ebbef
dzimentey <dzimentey@gmail.com>
2022-03-24 13:29:37 +0300

    refactor: add profileDescription variable, use it instead plain text

--- src/main/ui/AboutMe/AboutMe.tsx
+++ src/main/ui/AboutMe/AboutMe.tsx
@@ -22,6 +22,13 @@ const useStyles = makeStyles({
   },
 });
 
+
+const profileDescription: string =
+    `Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+     Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.`
+
+
 export const AboutMe = () => {
   const classes = useStyles();
   return (
@@ -46,9 +53,7 @@ export const AboutMe = () => {
             React developer
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              {profileDescription}
           </Typography>
         </CardContent>
         {/*</CardActionArea>*/}
